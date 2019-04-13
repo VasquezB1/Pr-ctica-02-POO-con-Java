a2bea95c491ff88253d8c41902019a0ab1f0b5fd
Byron PC <Byron PC@172.16.212.118>
2019-04-12 19:00:26 -0500

    Práctica 02 Programación Aplicada Declaracion de Clases Declaracion clases con sus atributos y metodos Declaracion de clases Abstractas

--- src/ec/edu/ups/clases/Empleados.java
+++ src/ec/edu/ups/clases/Empleados.java
@@ -12,9 +12,63 @@ import java.util.Date;
  * @version IDE 8.2
  * ----------Class Padre 1----------
  */ 
-public class Empleados extends Persona {
+public abstract class Empleados extends Persona {
+////////////////////////////////////////////////////////////////////////////////
+    ///////////////////Declaracion de Variables    
     private String empresa;
     private double sueldo;
     private Date fechaContratacion;
     private Date vacaciones;
+    
+    public Empleados(){
+        
+    }
+////////////////////////////////////////////////////////////////////////////////
+    //Constructor
+    public Empleados(String nombre, String cedula, int edad, String genero) {
+        super(nombre, cedula, edad, genero);
+    }
+////////////////////////////////////////////////////////////////////////////////
+    //Get and Set   
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public Date getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setFechaContratacion(Date fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+
+    public Date getVacaciones() {
+        return vacaciones;
+    }
+
+    public void setVacaciones(Date vacaciones) {
+        this.vacaciones = vacaciones;
+    }
+/////////////////////////////////////////////////////////////////////////////////
+    //To String
+
+    @Override
+    public String toString() {
+        return "Empleados{" + "empresa=" + empresa + ", sueldo=" + sueldo + ", fechaContratacion=" + fechaContratacion + ", vacaciones=" + vacaciones + '}';
+    }
+    
+    
 }
--- src/ec/edu/ups/clases/Profesor.java
+++ src/ec/edu/ups/clases/Profesor.java
@@ -10,10 +10,64 @@ package ec.edu.ups.clases;
  * @version IDE 8.2
  * ----------Class Padre 2----------
  */
-public class Profesor extends Persona{
+public abstract class Profesor extends Persona{
+////////////////////////////////////////////////////////////////////////////////
+    ///////////////////Declaracion de Variables    
     private String materia;
     private String colegio;
     private int codigo;
     private String turno;
+    
+    public Profesor(){
+        
+    }
+////////////////////////////////////////////////////////////////////////////////
+    //Constructor
+
+    public Profesor(String nombre, String cedula, int edad, String genero) {
+        super(nombre, cedula, edad, genero);
+    }
+////////////////////////////////////////////////////////////////////////////////
+    //Get and Set   
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public String getColegio() {
+        return colegio;
+    }
+
+    public void setColegio(String colegio) {
+        this.colegio = colegio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+/////////////////////////////////////////////////////////////////////////////////
+    //To String
+
+    @Override
+    public String toString() {
+        return "Profesor{" + "materia=" + materia + ", colegio=" + colegio + ", codigo=" + codigo + ", turno=" + turno + '}';
+    }
+
     
 }
